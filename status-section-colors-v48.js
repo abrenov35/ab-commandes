fe9f93b91761ff66aefbfe4ff50604ba65b5fd9a
abrenov35 <abrenov35@gmail.com>
2026-09-13 14:27:25 +0200

    AB COMMANDES - neutraliser ancienne V48 en cache

---
+++ status-section-colors-v48.js
@@ -0,0 +1,5 @@
+/* Compatibilité cache navigateur : ancienne page V48. volontairement neutre. */
+(function(){
+  'use strict';
+  window.__AB_COMMANDES_STATUS_SECTION_COLORS_VERSION='48-cache-neutral';
+})();
